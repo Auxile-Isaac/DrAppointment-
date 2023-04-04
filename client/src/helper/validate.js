371c9ba699d018f737fa3de30fe195f4ec9a3d90
Auxile_Isaac <auxile.isaac@gmail.com>
2023-04-04 20:46:22 +0200

    password component and it's validation are done

---
+++ client/src/helper/validate.js
@@ -0,0 +1,43 @@
+import toast  from "react-hot-toast"
+
+/** validate login page username**/
+export async function usernameValidate(values){
+    const errors = usernameVerify({}, values);
+
+    return errors;
+}
+
+/** validate Pasword page username**/
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+
+    return errors
+}
+
+/** validate Password **/
+function passwordVerify(errors = {}, values){
+
+    const specialChars = /[\W_]/g
+    if(!values.password){
+        errors.password = toast.error("Password Required..!");
+    }else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password..!")
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long..!")
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special characters ..!")
+    }
+
+    return errors
+}
+
+/** validate username **/
+function usernameVerify(errors = {}, values){
+    if(!values.username){
+        errors.username = toast.error("Username Required..!")
+    }else if(values.username.includes(" ")){
+        errors.username = toast.error("Invarid Username..!")
+    }
+
+    return errors
+}
